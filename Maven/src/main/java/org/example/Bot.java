de65a47f0e6745ccb5a9fdf6971d60be38d5415f
HarryOnFerrari <babakova.anast@gmail.com>
2021-10-17 12:47:27 +0500

    добавлены первые команды

--- Maven/src/main/java/org/example/Bot.java
+++ Maven/src/main/java/org/example/Bot.java
@@ -7,6 +7,13 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class Bot extends TelegramLongPollingBot {
 
+    private String HELP = "Це бот для обучения. " +
+            "Сейчас ты можешь проходить здесь тесты и проверять свой скилл. \n" +
+            "Просто используй: \n" +
+            "/test - для запуска теста \n" +
+            "/next - для перехода к следующему вопросу \n" +
+            "/stop - для завершения работы";
+
     @Override
     public String getBotUsername() {
         return "giveme100poinrsinbrs_bot";
@@ -23,8 +30,31 @@ public class Bot extends TelegramLongPollingBot {
             SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
             message.setChatId(update.getMessage().getChatId().toString());
             // setChatId - id клиента, обратившегося к боту
-            message.setText(update.getMessage().getText());
+            //message.setText(update.getMessage().getText());
             //message.setText("Хорошего дня, реальный кабан!");
+
+            switch (update.getMessage().getText()){
+                case ("/start"):
+                    message.setText("Привет, работяга!");
+                    break;
+
+                case ("/test"):
+                    message.setText("И тут начинаются тесты");
+                    break;
+
+                case ("/help"):
+                    message.setText(HELP);
+                    break;
+
+                case ("/stop"):
+                    message.setText("Эта функция будет реализована в будущих версиях :)))");
+                    break;
+
+                default:
+                    message.setText("Такой команды пока не существует, или Вы допустили ошибку в написании. " +
+                            "Воспользуйтесь командой /help, чтобы прочитать инструкцию.");
+                    break;
+            }
 
             try {
                 execute(message); // Call method to send the message
