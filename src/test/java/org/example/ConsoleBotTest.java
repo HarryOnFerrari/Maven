7fe614107d3b3f3793c7d4d41d81714e837121b6
HarryOnFerrari <babakova.anast@gmail.com>
2021-11-04 02:46:59 +0500

    Новый интерфейс

--- src/test/java/org/example/ConsoleBotTest.java
+++ src/test/java/org/example/ConsoleBotTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.PrintStream;
 import java.util.Scanner;
 
@@ -16,38 +17,38 @@ public class ConsoleBotTest {
     /**
      * Проверка бота на корректный выход из режима тестов.
      *
+     * @exception IOException
      * @result По завершению списка вопросов пользователь получает сообщение,
      *         ошибок не возникает.
      */
     @Test
-    public void repeatingTests()
-    {
+    public void repeatingTests() throws IOException {
         ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         String str = "/test";
         for (Integer i=0; i<=149; i++){
             str += "\n/next";
         }
-        //str += "/stop";
         ByteArrayInputStream in = new ByteArrayInputStream(str.getBytes());
         ConsoleBot bot = new ConsoleBot(new Scanner(in), new PrintStream(outContent));
         bot.run();
+        in.close();
         String[] result = outContent.toString().split("\r\n");
         assertEquals("Вопросов нет.", result[result.length-1]);
-        System.setOut(null);
     }
 
     /**
      * Проверка на то, что правильный ответ будет принят с любым регистром написания.
      *
+     * @exception IOException
      * @result Корректная оценка ботом ответа пользователя.
      */
     @Test
-    public void checkRegister()
-    {
+    public void checkRegister() throws IOException {
         ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         ByteArrayInputStream in = new ByteArrayInputStream("/test\nПрОсТоЙ".getBytes());
         ConsoleBot bot = new ConsoleBot(new Scanner(in), new PrintStream(outContent));
         bot.run();
+        in.close();
         assertEquals("Правильный ответ!", outContent.toString().split("\r\n")[1]);
     }
 }
