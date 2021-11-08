e5989fede7d78eefbd2c14080035f7c1e8c0157b
life-on-loan <79449918+life-on-loan@users.noreply.github.com>
2021-11-08 20:58:30 +0500

    update

--- src/main/resources/Карточки русский язык егэ словарные слова _ Quizlet_files/config.js
+++
@@ -1,5 +0,0 @@
-
-window[window["TiktokAnalyticsObject"]].instance("C3E8DRI7V1C28HV6V6D0").setAdvancedMatchingAvailableProperties({"email":false,"phone_number":false});
-window[window["TiktokAnalyticsObject"]].instance("C3E8DRI7V1C28HV6V6D0").setPixelInfo && window[window["TiktokAnalyticsObject"]].instance("C3E8DRI7V1C28HV6V6D0").setPixelInfo({status: 0, name: "Quizlet TikTok Pixel", advertiserID: "6875367554253062145", setupMode: 0, partner: "", is_onsite: false});
-window[window["TiktokAnalyticsObject"]].setPCMDomain && window[window["TiktokAnalyticsObject"]].setPCMDomain("quizlet.com");
-window[window["TiktokAnalyticsObject"]].setPCMConfig && window[window["TiktokAnalyticsObject"]].setPCMConfig([]);
