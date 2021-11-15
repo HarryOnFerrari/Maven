94c4cd3217e39814546329bbec3e917319b071e1
life-on-loan <79449918+life-on-loan@users.noreply.github.com>
2021-11-15 15:50:59 +0500

    setting notification finish

--- src/main/java/org/example/utils/UpdateTimeNotification.java
+++ src/main/java/org/example/utils/UpdateTimeNotification.java
@@ -5,12 +5,10 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class UpdateTimeNotification {
-    public Date timeUp(Date today, int sdvig) {
+    public Date timeUp(Date today, int offset) {
         Calendar gCalendar = new GregorianCalendar();
-        //System.out.println(gCalendar.getTime());
         gCalendar.setTime(today);
-        gCalendar.add(Calendar.SECOND,30);
-        //System.out.println(gCalendar.getTime());
+        gCalendar.add(Calendar.DAY_OF_YEAR, offset);
         return gCalendar.getTime();
     }
 }
