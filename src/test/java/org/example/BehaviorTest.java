71ccceccb0075c612bf6b8e91344df5ed6f7aa57
HarryOnFerrari <vi-chan@mail.ru>
2021-12-08 17:21:05 +0500

    Перенос тестов с мокито

--- src/test/java/org/example/BehaviorTest.java
+++ src/test/java/org/example/BehaviorTest.java
@@ -73,4 +73,41 @@ public class BehaviorTest
         Assert.assertEquals(questionWithWrongAnswer,
                 fakeBot.getMessages().get(6));
     }
+
+    /**
+     * Отладочный тест для проверки того, что, после прохождения теста по одному предмету, результат
+     * последней попытки не переносится в статистику другого предмета (больная мозоль)
+     */
+    @Test
+    public void checkUserStatisticsLogic(){
+        FakeBot fakeBot = new FakeBot();
+        Behavior behavior = new Behavior(fakeBot);
+        List<String> commands = List.of("MATHS", "/test", "100", "/stop", "ENGLISH", "/statistic_subject");
+        for (String command : commands){
+            behavior.processCommand(user, command);
+        }
+        Assert.assertEquals("ENGLISH: Информации нет. Пройдите тест.",
+                fakeBot.getMessages().get(fakeBot.getMessages().size()-1));
+    }
+
+    /**
+     * Проверка своевременной рассылки напоминания большому числу пользователей
+     */
+    @Test
+    public void remind() throws InterruptedException {
+        FakeBot fakeBot = new FakeBot();
+        Behavior behavior = new Behavior(fakeBot);
+        TimerBehavior.setStandardDispatchTime(10);
+        int users = 10;
+        for (long i = 0L; i<users; i++) {
+            User user = new User(i);
+            behavior.processCommand(user, "timerOn");
+        }
+        Thread.sleep( 10 + users*20); // отправка 1 сообщения у fakeBot занимет примерно 20ms
+        String questionWithWrongAnswer = "Вас давно не было видно. Хотите пройти тест?";
+        Assert.assertEquals(questionWithWrongAnswer,
+                fakeBot.getMessages().get(fakeBot.getMessages().size() - 2));
+        Assert.assertEquals(questionWithWrongAnswer,
+                fakeBot.getMessages().get(fakeBot.getMessages().size() - 30 - 2));
+    }
 }
